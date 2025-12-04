f6440176f4d19ed913ea740ac3928ac9ccd43838
shaffatunnabi <alihassan282001@gmail.com>
2025-12-04 22:40:35 +0500

    Initial commit - Version 1

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+
+
+import { Link } from "expo-router";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import Logo from '../assets/img/dairy.png';
+const App = () => {
+  return (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center',backgroundColor:'#006D5B'}}>
+        <Image source={Logo} style={{width:150, height:150,marginTop:200}} />
+      <Text style={{fontSize:30,marginTop:30,color:'#ffff',ontStyle:'Bold'}}>AI DairyLens</Text>
+      <TouchableOpacity style={{marginTop:200, padding:10, backgroundColor:'#40B5AD', borderRadius:15,width:'60%', alignItems:'center'}}>
+        <Link href="/Authentication">
+          <Text style={{color:'#ffff',fontSize:20,fontStyle:'Bold'}}>Get Started</Text>
+        </Link>
+      </TouchableOpacity>
+    </View>
+  );
+};
+export default App;
+
+
+
+
+
+
